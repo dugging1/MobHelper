024c708e5f331956ab52bb3bd806c4282cf9140a
dugging <goldminergaming@aol.com>
2019-08-29 18:36:04 +0100

    Crimson Bloodline

---
+++ CrimsonBloodlineMobs/Cornfig.cs
@@ -0,0 +1,44 @@
+﻿using MobHelper.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LanguageExt;
+using static LanguageExt.Prelude;
+
+namespace CrimsonBloodlineMobs {
+	class Cornfig : IMob {
+
+		public Cornfig() {
+			Components = ImmutableHashSet.Create<IComponent>(new MobStats());
+			IStats s = (IStats)getComponent(typeof(IStats));
+			s.Strength = 10;
+			s.Dexterity = 10;
+			s.Intelligence = 10;
+			s.Wisdom = 10;
+			s.Constitution = 10;
+			s.Charisma = 10;
+			s.Speed = 30;
+			s.HP = MobRollHelper.roll(5, 8) + 10;
+			s.AC = 11;
+		}
+		public Cornfig(string name, int number, ImmutableHashSet<IComponent> comps) {
+			Name = name;
+			Number = number;
+			Components = comps;
+		}
+
+		private string name = "Cornfig";
+		public string Name { get => name; set { name = value; } }
+
+		public int Number { get; set; }
+
+		public ImmutableHashSet<IComponent> Components { get; private set; }
+
+		public IComponent getComponent(Type t) => Components.Filter(x => t.IsInstanceOfType(x)).HeadOrNone().Match(x => x, () => throw new ArgumentException("Component not found."));
+
+		public IMob New(string name, int number, ImmutableHashSet<IComponent> comps) => new Cornfig(name, number, comps);
+	}
+}
---
+++ CrimsonBloodlineMobs/Program.cs
@@ -0,0 +1,22 @@
+﻿using MobHelper.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrimsonBloodlineMobs {
+	static class Program {
+		[STAThread]
+		static void Main(string[] args) {
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			MainForm mf = new MainForm();
+			//mf.FormBorderStyle = FormBorderStyle.FixedDialog;
+
+			Application.Run(mf);
+		}
+	}
+}
